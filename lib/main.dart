2056cf6c61054f9c2c76a989461a54e2dbe6d3e0
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-08-14 13:39:10 -0300

    Trocando de Tela pelo DRAWER

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xlo/blocs/drawer_bloc.dart';
 import 'package:xlo/screens/base/base_screen.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -7,13 +10,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'XLO',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        Provider<DrawerBloc>(
+          create: (_) => DrawerBloc(),
+          dispose: (context, value) => value.dispose(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'XLO',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: BaseScreen(),
       ),
-      home: BaseScreen(),
     );
   }
 }
