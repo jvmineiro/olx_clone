2411606f1f214ac50f8574889dd0be351cd0b3fa
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-10-27 18:59:42 -0300

    Quase finalizando os anuncios na TELA

---
+++ lib/screens/home/widgets/product_tile.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:xlo/models/ad.dart';
+
+class ProductTile extends StatelessWidget {
+
+  ProductTile(this.ad);
+
+  final Ad ad;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+
+      },
+      child: Container(
+        height: 135,
+        margin: const EdgeInsets.symmetric(horizontal: 2, vertical: 1),
+        child: Card(
+          child: Row(
+            children: <Widget>[
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
